635c3df225c231257eac28f8b39ae2f47603f34a
Rafal <ravmaster48@gmail.com>
2021-06-06 17:40:17 +0100

    first commit

---
+++ src/main/java/com/rav/RavBookingApplication.java
@@ -0,0 +1,13 @@
+package com.rav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RavBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rav/RavBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
